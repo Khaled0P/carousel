9b7a788f83c33d7a6f2de326eb25916405afe4df
Khaled AbdRahem <aa.khaled11@gmail.com>
2023-04-28 03:44:17 +0300

    add: add progress buttons
    add buttons to show carousel content progress

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const slides = document.querySelectorAll('.slidesContainer img');
 //buttons
 const prevBtn = document.getElementById('prevBtn');
 const nextBtn = document.getElementById('nextBtn');
+const progressBtns = document.querySelectorAll('.slideBtns');
 
 //scroll by size
 const size = slides[0].clientWidth;
@@ -14,12 +15,23 @@ let counter = 1;
 
 function scrolSlide() {
   slidesContainer.style.transform = `translateX(${-size * counter}px)`;
+  //progessBtns loop
+  if (progressBtns[counter].className === 'slideBtns first') {
+    progressBtns[1].classList.add('active');
+  } else if (progressBtns[counter].className === 'slideBtns last') {
+    progressBtns[slides.length - 2].classList.add('active');
+  } else {
+    progressBtns[counter].classList.add('active');
+  }
 }
 
 scrolSlide();
 
 nextBtn.addEventListener('click', () => {
   if (counter >= slides.length - 1) return;
+  progressBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
   slidesContainer.style.transition = 'transform .4s ease-in-out';
   counter++;
   scrolSlide();
@@ -27,15 +39,19 @@ nextBtn.addEventListener('click', () => {
 
 prevBtn.addEventListener('click', () => {
   if (counter <= 0) return;
+  progressBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
   slidesContainer.style.transition = 'transform .4s ease-in-out';
   counter--;
   scrolSlide();
 });
 
+//carousel loop
 slidesContainer.addEventListener('transitionend', () => {
   if (slides[counter].className === 'last') {
-    slidesContainer.style.transition = 'none';
     counter = slides.length - 2;
+    slidesContainer.style.transition = 'none';
     scrolSlide();
   } else if (slides[counter].className === 'first') {
     slidesContainer.style.transition = 'none';
